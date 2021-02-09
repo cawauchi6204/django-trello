69cee20a690b0d51f6b268db9fc1b2ed01d281d8
cawauchi6204 <cawauchi6204@gmail.com>
2021-02-09 21:45:08 +0900

    first commit

---
+++ config/urls.py
@@ -0,0 +1,26 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("kanban/", include("kanban.urls")),
+    path('kanban/', include("django.contrib.auth.urls")),
+    path('admin/', admin.site.urls),
+]
+
+# kanbanアプリのurls.pyとは別に、djangoの認証系のルーティング設定をincludeで入れ子にしています。
+# このようにすることで、下記のようなルーティング設定になります。
---
+++ kanban/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+app_name = "kanban"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("home/", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+]
+
+# urlpatterns というリストに path() 関数を格納します。
+# path() 関数は route , view , name , kwargs の4つの引数を受け取ります。
+# そのうち route と view の2つは必須の引数で、他の2つは省略可能な引数です。
+# route はURLパターンの文字列です。Djangoはリクエストとして受け取ったURLと、routeとして設定された文字列をリストの上から順に比較し、一致するものを特定します。（厳密にはドメイン以下を比較します）
+# 例えばドメインを https://www.example.com/ とすると、今指定したパターンは ''（空の文字列） なので https://www.example.com/ そのものが特定されます。 https://www.example.com/users や https://www.example.com/home は除外されます。
+# view はビュー関数を指定します。 route で指定したURLパターンで特定されたリクエストは view で指定したビューに渡されます。
+# name に文字列を指定することで、URLの名称の定義が可能です。これにより、Djangoのあらゆる場所から参照することが可能となります。
+# kwargs は追加の引数を辞書としてビューに渡すことができます。
+# ここでは route と view name を指定しています。
